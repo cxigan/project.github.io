6efbfd07d3c86e2b2b61b43803f2341f08844c11
cxigan <98639011+cxigan@users.noreply.github.com>
2025-05-26 18:19:37 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # project1
-https://cxigan.github.io/project.github.io/
--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggleButton = document.getElementById('menu-toggle');
-  const navMenu = document.querySelector('.nav');
+document.addEventListener('DOMContentLoaded', function () { //вся HTML-страница будет полностью загружена и готова к работе, тогда ког будет выполняться
+  const toggleButton = document.getElementById('menu-toggle'); // получаем кнопку бургер-меню по её ID
+  const navMenu = document.querySelector('.nav'); //находим навигационное меню по классу.
 
   toggleButton.addEventListener('click', function () {
-    navMenu.classList.toggle('active');
+    navMenu.classList.toggle('active'); // переключает (добавляет/удаляет) класс active у меню при каждом клике на кнопку.
   });
 });
 
 document.getElementById("downloadBtn").addEventListener("click", function () {
 
   // Создаём невидимую ссылку и "кликаем" по ней
-  const link = document.createElement("a");
+  const link = document.createElement("a"); // получаем кнопку скачивания по ID.
   link.href = "quiz.pdf"
   link.download = "quiz.pdf"; // Без этого файл может открыться вместо скачивания!
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
+  document.body.appendChild(link);//
+  link.click();//программно "кликаем" по ссылке, запуская скачивание
+  document.body.removeChild(link);//удаляем ссылку из DOM после скачивания
 });
